12387e81fd44b57bd7492d39ef2b710a6b915e41
Many92 <manycuba92@gmail.com>
2019-10-14 17:16:50 -0400

    uniendo proyectos 15-10-2019

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize , DataTypes) =>{
+    const tabla = sequelize.define('Usuario',{
+        id:{
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        username:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        password:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        email:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                isEmail:true
+            }
+        }
+    },{
+        tableName:'usuario'
+    });
+    tabla.associate = (models)=>{
+        tabla.belongsTo(models.Rol);
+    }
+
+    return tabla;
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
+/* eslint-disable vue/valid-v-else-if */
 module.exports = app => {
 
-    app.get('/',(req,res)=>{
-        res.json({status:"Api CtEnpa",entity:"UEB ENPA IJ"})
+    app.get('/', (req, res) => {
+        res.json({
+            status: "Api CtEnpa",
+            entity: "UEB ENPA IJ"
+        })
     })
 
 }
