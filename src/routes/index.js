94b253bca5b5cca1be999cee7bda9512a5eccb3a
OsmanyCuba <manycuba92@gmail.com>
2019-10-21 15:04:30 -0400

    modificando el index.js y el app.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,6 @@
-import path from 'path'
+// import path from 'path'
 
 module.exports = app => {
     // ruta para la vista en caso de que no exista la petición en el router de express
-    app.get('*', (req, res) => {
-        res.sendfile(path.resolve('src/public/index.html'));
-    });
+
 }
