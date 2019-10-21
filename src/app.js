56a6d0ae82a71d8c010e33ea763afe5118eaee79
Rayco García <rayco.garcia13@gmail.com>
2019-10-21 14:29:31 -0400

    arreglando el app.js

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(express.static(__dirname + '/public'))
 
 consign({
-    .include('./libs/config.js')
-    .then('./db.js')
-    .then('./libs/middlewares.js')
-    .then('./controllers')
-    .then('./routes')
-    .then('./libs/boot.js')
-    .into(app);
+    cwd: __dirname
+})
+.include('./libs/config.js')
+.then('./db.js')
+.then('./libs/middlewares.js')
+.then('./controllers')
+.then('./routes')
+.then('./libs/boot.js')
+.into(app);
