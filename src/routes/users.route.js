83e15103d0fc084734057bcc5c7addc5f71f580c
OsmanyCuba <manycuba92@gmail.com>
2019-10-21 14:42:04 -0400

    comprobando

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -5,34 +5,34 @@ module.exports = {
         res.status(200) // 200 => Todo está O.K.
         return usuarios;
     },
-    loadTasks: async (req, res) => {
-        const Entries = await control.getAlimentos()
-        res.status(200) // 200 => Todo está O.K.
-        res.json(Entries);
+    // loadTasks: async (req, res) => {
+    //     const Entries = await control.getAlimentos()
+    //     res.status(200) // 200 => Todo está O.K.
+    //     res.json(Entries);
 
-    },
-    loadTask: async (req, res) => {
-        const Entry = await control.getAlimentobyId(req.params.id)
-        res.status(200)
-        res.json(Entry)
-    },
-    newAlimento: async (req, res) => {
-        const newEntry = await control.createOrUpdateAlimento(req.body)
-        res.status(201) // 201 => Hay nuevo contenido.        
-        res.json(newEntry)
+    // },
+    // loadTask: async (req, res) => {
+    //     const Entry = await control.getAlimentobyId(req.params.id)
+    //     res.status(200)
+    //     res.json(Entry)
+    // },
+    // newAlimento: async (req, res) => {
+    //     const newEntry = await control.createOrUpdateAlimento(req.body)
+    //     res.status(201) // 201 => Hay nuevo contenido.        
+    //     res.json(newEntry)
 
-    },
+    // },
 
-    updateAlimento: async (req, res) => {
-        const id = req.params.id;
-        const updatedEntry = await control.createOrUpdateAlimento(req.body)
-        // console.log(req.body);
-        res.status(201)
-        res.json(updatedEntry)
-    },
-    deleteAlim: async (req, res) => {
-        const deletedEntry = await control.deleteAlimentos(req.params.id)
-        res.status(204) // 204 => No existe contenido.
-        res.json(deletedEntry)
-    }
+    // updateAlimento: async (req, res) => {
+    //     const id = req.params.id;
+    //     const updatedEntry = await control.createOrUpdateAlimento(req.body)
+    //     // console.log(req.body);
+    //     res.status(201)
+    //     res.json(updatedEntry)
+    // },
+    // deleteAlim: async (req, res) => {
+    //     const deletedEntry = await control.deleteAlimentos(req.params.id)
+    //     res.status(204) // 204 => No existe contenido.
+    //     res.json(deletedEntry)
+    // }
 }
